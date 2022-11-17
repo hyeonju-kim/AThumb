8c17a8709391e267be61886d96cd32ba5ae640b3
JSU0318 <dnr0000@gmail.com>
2022-11-17 15:38:37 +0900

    1차 검토

--- app_hj.py
+++ app_hj.py
@@ -211,5 +211,6 @@ def api_valid():
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5005, debug=True)
